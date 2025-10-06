b2eaefde04ae412f4332c0e8a41f2bb62d86982d
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2025-10-06 09:57:46 +0700

    add wishlist table

--- src/main/java/com/roofiahmad/store/StoreApplication.java
+++ src/main/java/com/roofiahmad/store/StoreApplication.java
@@ -1,22 +1,14 @@
 package com.roofiahmad.store;
 
-import com.roofiahmad.store.entities.User;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-//		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		var user = User.builder().name("roofiahmad").password("password").email("email").build();
-//		var address = Address.builder().street("street").city("city").state("state").zip("zip").build();
-//		user.addAddress(address);
-
-//		user.addTag("tag1");
-//		var profile = Profile.builder().bio("bio").build();
-//		user.setProfile(profile);
-//		profile.setUser(user);
-//		System.out.printf("User: %s\n", user);
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
 	}
 }
