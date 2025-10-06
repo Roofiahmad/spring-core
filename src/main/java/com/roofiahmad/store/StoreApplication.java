28861650134ac25d3754f5342ed5d7a21623c3a0
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2025-10-06 08:18:57 +0700

    add many to many relationship (user - tag)

--- src/main/java/com/roofiahmad/store/StoreApplication.java
+++ src/main/java/com/roofiahmad/store/StoreApplication.java
@@ -1,6 +1,5 @@
 package com.roofiahmad.store;
 
-import com.roofiahmad.store.entities.Address;
 import com.roofiahmad.store.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,10 +9,12 @@ public class StoreApplication {
 	public static void main(String[] args) {
 //		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 		var user = User.builder().name("roofiahmad").password("password").email("email").build();
-		var address = Address.builder().street("street").city("city").state("state").zip("zip").build();
+//		var address = Address.builder().street("street").city("city").state("state").zip("zip").build();
+//		user.addAddress(address);
 
-		user.addAddress(address);
-
+		user.addTag("tag1");
 		System.out.printf("User: %s\n", user);
+
+
 	}
 }
--- src/main/java/com/roofiahmad/store/entities/Tag.java
+++ src/main/java/com/roofiahmad/store/entities/Tag.java
@@ -1,10 +1,18 @@
 package com.roofiahmad.store.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "tags")
 public class Tag {
@@ -17,4 +25,11 @@ public class Tag {
     @Column(name="name")
     private String name;
 
+    @ManyToMany(mappedBy = "tags")
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
 }
